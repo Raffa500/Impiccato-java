634126e49ffb545e53ad7bdd90fed1786bc08ace
elubr <elubr@DESKTOP-0MKHJIP.wind3.hub>
2023-01-08 21:07:36 +0100

    impiccato

---
+++ src/gioco/indovina/la/parola/GiocoIndovinaLaParola.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gioco.indovina.la.parola;
+
+/**
+ *
+ * @author elubr
+ */
+public class GiocoIndovinaLaParola {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
